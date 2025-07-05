3e10d579d9ce538f2b77adfb43f04382b2b58c41
lc631017672 <631017672@qq.com>
2025-07-06 02:28:05 +0800

    feat: 添加 API 启动前的清理操作
    在 start_api.sh 脚本中添加了以下清理操作：
    - 删除上次生成的音频文件。
    - 清理 /tmp/gradio/ 目录下的文件。
    - 删除 /tmp/ 目录下所有空目录。
    
    这些操作有助于确保每次 API 启动时环境的清洁，避免旧文件或临时文件干扰。

--- fastapi_app/start_api.sh
+++ fastapi_app/start_api.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#删除上次音频文件
+rm -rf /workspace/index-tts-api/outputs/*  
+
+cd /tmp/ && find . -empty -type d -delete 
+
+rm -rf /tmp/gradio/* 
 source $(conda info --base)/etc/profile.d/conda.sh
 conda activate index-tts
 python main.py
